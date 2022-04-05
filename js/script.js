15e8eb42f1424e62cdd9bbb00c288ceee4100316
Aloinopsis <darekgumula@gmail.com>
2022-04-05 20:00:30 +0200

    JS-zmiana koloru barsa, aktualny rok footer-KONIEC

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 // wyświetlanie i chowanie navigacji
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
+	navBtnBars.classList.remove("brown-bars-color");
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
 			nav.classList.remove("nav--active");
@@ -25,4 +29,28 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add("brown-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove("brown-bars-color");
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleObserver);
